5580fdf69cc52f99be7d88f404d245630879bd81
gaurav <theweirdki9@gmail.com>
2024-06-14 14:49:13 +0530

    Added bcrypt functionality

--- src/services/bcrypt.js
+++ src/services/bcrypt.js
@@ -1,2 +1,25 @@
 //hash and compare passwords
+import bcrypt from 'bcrypt';
+const saltRounds = parseInt(process.env.SALT) || 10;
 
+const HashPassword = async (plainPassword) => {
+    try {
+        const hash = await bcrypt.hash(plainPassword, saltRounds);
+        return hash;
+    } catch(error) {
+        console.error("Error Hashing Password: ", error)
+        throw error
+    }
+}
+
+const CheckPassword = async (plainPassword, hashedPassword) => {
+    try {
+        const valid = await bcrypt.compare(plainPassword, hashedPassword);
+        return valid;
+    } catch(error) {
+        console.error("Error checking Password: ", error)
+        throw error;
+    }
+}
+
+export {HashPassword, CheckPassword}
